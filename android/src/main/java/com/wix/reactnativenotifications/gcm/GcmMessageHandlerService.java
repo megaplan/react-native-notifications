d70e0063d10093104f1fbb1ec0793c13563fa993
Igor Sukhoverkhov <i.sukhoverkhov@megaplan.ru>
2018-05-29 13:20:12 +0300

    Android 8 GCM fix

--- android/src/main/java/com/wix/reactnativenotifications/gcm/GcmMessageHandlerService.java
+++ android/src/main/java/com/wix/reactnativenotifications/gcm/GcmMessageHandlerService.java
@@ -1,26 +1,62 @@
 package com.wix.reactnativenotifications.gcm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.JobIntentService;
+import android.content.Context;
 import android.util.Log;
 
-import com.google.android.gms.gcm.GcmListenerService;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.wix.reactnativenotifications.core.notification.IPushNotification;
 import com.wix.reactnativenotifications.core.notification.PushNotification;
 
 import static com.wix.reactnativenotifications.Defs.LOGTAG;
 
-public class GcmMessageHandlerService extends GcmListenerService {
+public class GcmMessageHandlerService extends JobIntentService
+{
+    /**
+     * Unique job ID for this service.
+     */
+    static final int JOB_ID = 1000;
+
+    /**
+     * Convenience method for enqueuing work in to this service.
+     */
+    public static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, GcmMessageHandlerService.class, JOB_ID, work);
+    }
 
     @Override
-    public void onMessageReceived(String s, Bundle bundle) {
-        Log.d(LOGTAG, "New message from GCM: " + bundle);
+    protected void onHandleWork(Intent intent)
+    {
 
-        try {
-            final IPushNotification notification = PushNotification.get(getApplicationContext(), bundle);
-            notification.onReceived();
-        } catch (IPushNotification.InvalidNotificationException e) {
-            // A GCM message, yes - but not the kind we know how to work with.
-            Log.v(LOGTAG, "GCM message handling aborted", e);
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging service = GoogleCloudMessaging.getInstance(this);
+
+        String messageType = service.getMessageType(intent);
+
+        Log.d("PUSH", "GCMIntentService received message type : " + messageType);
+
+        if (!extras.isEmpty())
+        {
+            if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType))
+            {
+                try {
+                    final IPushNotification notification = PushNotification.get(getApplicationContext(), extras);
+                    notification.onReceived();
+                } catch (IPushNotification.InvalidNotificationException e) {
+                    // A GCM message, yes - but not the kind we know how to work with.
+                    Log.v(LOGTAG, "GCM message handling aborted", e);
+                }
+
+            }
         }
     }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+    }
+
 }
